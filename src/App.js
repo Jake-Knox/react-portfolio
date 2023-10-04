a2fad592e2b352cb7cbde5234aaa5a2c8eb4cb6a
jake-knox- <jakeknox11@hotmail.co.uk>
2023-10-04 12:50:08 +0100

    added tailwindcss, basic components setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <body className="App-body">
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
-      </body>
-    </div>
+export default function App() {
+  return (
+    <main className='text-gray-400 bg-gray-900 body-font'>
+      <h1>test</h1>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>    
   );
 }
 
-export default App;
