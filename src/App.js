237dff2b98b13969e9ff2519eaa47726d8e8ff87
jake-knox- <jakeknox11@hotmail.co.uk>
2023-10-05 11:31:27 +0100

    added data and images, finished about

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
+// https://www.freecodecamp.org/news/build-portfolio-website-react/
+
 export default function App() {
   return (
     <main className='text-gray-400 bg-gray-900 body-font'>
