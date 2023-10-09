258781e8ee30655d36eacaca0a48eaa39c77ef65
jake-knox- <jakeknox11@hotmail.co.uk>
2023-10-09 16:20:22 +0100

    perosnalising work

--- src/components/About.js
+++ src/components/About.js
@@ -7,23 +7,21 @@ export default function About() {
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, I'm Jake.
-                        <br className="inline-block"/>I love to build amazing
-                        apps.
+                        <br className="inline-block" />
+                        I'm a Software Developer
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                        Laborum, voluptas natus?
+                        Manchetser based software developer with a degree in Computer Science. My interests range from Coding to Running, Reading, Films and Music.
                     </p>
                     <div className="flex justify-center">
                         <a
-                         href="#contact"
-                         className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            href="#contact"
+                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg text-center">
                             Contact Me
                         </a>
                         <a
-                        href="#projects"
-                        className="ml-4 inline-flex text-fray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            href="#projects"
+                            className="ml-4 inline-flex text-fray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg text-center">
                             See My Past Work
                         </a>
                     </div>
@@ -32,7 +30,7 @@ export default function About() {
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
-                        src="./bookends.png"
+                        src="./JakeKnoxProfileCrop.jpg"
                     />
                 </div>
             </div>
