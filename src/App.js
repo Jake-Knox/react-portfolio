8df09ee0b04afeb1db1dd83dd610041af27953eb
jake-knox- <jakeknox11@hotmail.co.uk>
2023-10-26 16:54:11 +0100

    temp removing testimonials

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </main>
